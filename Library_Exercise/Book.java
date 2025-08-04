87a05e81079bb64b375fa1fd237c73758c93db53
Hoursesoft <martinezmunozbreyners@gmail.com>
2025-08-04 23:58:42 +0000

    Se dio inicio a  los ejercicios de practica poo

---
+++ Library_Exercise/Book.java
@@ -0,0 +1,20 @@
+public class Book
+{
+    private String title;
+    private String author;
+    private Double price;
+
+    public Book (String title, String author, Double price)
+    {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+
+    }
+
+    public String toString()
+    {
+        return "{" + title + "}"+ "{"+ author + "}" + "{" + price + "}";
+    }
+}
+
